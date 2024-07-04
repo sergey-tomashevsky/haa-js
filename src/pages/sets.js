fdab38500ab76f2ef2ad4134a75673a11624e3ab
Sergey Tomashevsky <blackrid2011@gmail.com>
2024-07-04 19:46:56 +0600

    Separated mutation observers on SetsPage

--- src/pages/sets.js
+++ src/pages/sets.js
@@ -2,6 +2,8 @@ import Page from './page';
 
 export default class SetsPage extends Page {
   run()  {
+    const wrapper = document.getElementById('wrapper');
+
     const observer = new MutationObserver(() => {
       const setsContainer = document.getElementById('setsContainer');
       if (!setsContainer) return;
@@ -13,9 +15,19 @@ export default class SetsPage extends Page {
       this.#highlightMarketHeaderLink();
     });
 
-    const wrapper = document.getElementById('wrapper');
     observer.observe(wrapper, { childList: true, subtree: true });
     this._observers.push(observer);
+
+    const menuObserver = new MutationObserver(() => {
+      const dulstMenu = document.getElementById('dulstMenu');
+      if (!dulstMenu) return;
+
+      if (dulstMenu.querySelector('.customElement')) return;
+
+      this.#highlightMarketHeaderLink();
+    });
+    menuObserver.observe(wrapper, { childList: true, subtree: true });
+    // TODO: can amount of observers be optimized?
   }
 
   #updateMarketLink(container) {
