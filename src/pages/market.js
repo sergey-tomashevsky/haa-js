426a229e4ef7e978e47ea3555d27146d82c73e2a
Sergey Tomashevsky <blackrid2011@gmail.com>
2024-07-04 19:47:24 +0600

    Removed logs from MarketPage

--- src/pages/market.js
+++ src/pages/market.js
@@ -3,11 +3,9 @@ import Page from './page';
 export default class MarketPage extends Page {
   run()  {
     const observer = new MutationObserver((mutation) => {
-      console.log('MarketPage: mutation observed', mutation);
       const marketContainer = document.getElementById('marketContainer');
       if (!marketContainer) return;
 
-      console.log('MarketPage: marketContainer detected');
       // Skip updating if custom elements already exist.
       if (marketContainer.querySelector('.customElement')) return;
 
@@ -17,7 +15,6 @@ export default class MarketPage extends Page {
       setsLink.classList.add('customElement');
       setsLink.innerHTML = 'Sets';
       marketContainer.querySelector('header.page menu.secondaryMenu').append(setsLink);
-      console.log('MarketPage: button added');
     });
 
     const wrapper = document.getElementById('wrapper');
