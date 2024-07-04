52b04df8b896a081485216862fc5ba510b7dd794
Sergey Tomashevsky <blackrid2011@gmail.com>
2024-07-04 19:19:48 +0600

    Added extra logs to MarketPage

--- src/pages/market.js
+++ src/pages/market.js
@@ -2,10 +2,12 @@ import Page from './page';
 
 export default class MarketPage extends Page {
   run()  {
-    const observer = new MutationObserver(() => {
+    const observer = new MutationObserver((mutation) => {
+      console.log('MarketPage: mutation observed', mutation);
       const marketContainer = document.getElementById('marketContainer');
       if (!marketContainer) return;
 
+      console.log('MarketPage: marketContainer detected');
       // Skip updating if custom elements already exist.
       if (marketContainer.querySelector('.customElement')) return;
 
@@ -14,8 +16,8 @@ export default class MarketPage extends Page {
       setsLink.href = `/${gameSubdomain}/sets`;
       setsLink.classList.add('customElement');
       setsLink.innerHTML = 'Sets';
-
       marketContainer.querySelector('header.page menu.secondaryMenu').append(setsLink);
+      console.log('MarketPage: button added');
     });
 
     const wrapper = document.getElementById('wrapper');
