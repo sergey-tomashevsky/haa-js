50b3403ad46bf55da0a9fe8265bd42e4c7505d82
Sergey Tomashevsky <blackrid2011@gmail.com>
2024-07-07 21:14:42 +0600

    Added cost render to CardsPage

--- src/pages/cards.js
+++ src/pages/cards.js
@@ -6,16 +6,29 @@ export default class CardsPage extends Page {
       const cardsContainer = document.getElementById('cardsList');
       if (!cardsContainer) return;
 
-      // Skip updating if custom elements already exist.
-      if (cardsContainer.querySelector('.customElement')) return;
-
       const cardsByIndex = window.ty.fullCardsListIndex.nid;
       cardsContainer.querySelectorAll('#card-list-container .card:not(.customElement)').forEach((card) => {
         const properties = document.createElement('div');
+        properties.classList.add('properties');
         card.append(properties);
+
         const cardImage = card.querySelector('img');
         cardImage.setAttribute('loading', 'lazy');
         card.classList.add('customElement');
+
+        const cardInfo = cardsByIndex[card.getAttribute('data-cardid')];
+        if (!cardInfo) return;
+
+        // Add card cost.
+        if (cardInfo.cost) {
+          const cost = document.createElement('div');
+          cost.setAttribute('data-name', 'cost');
+          cost.setAttribute('data-value', cardInfo.cost + '');
+          cost.classList.add('property');
+          cost.classList.add('mainProperty');
+          cost.innerText = cardInfo.cost;
+          properties.append(cost);
+        }
       });
     });
 
