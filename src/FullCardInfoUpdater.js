7a292ef60d7c9035aedb4efdb750784fec87caa1
Sergey Tomashevsky <blackrid2011@gmail.com>
2024-07-19 06:13:15 +0600

    Extracted card name font size update logic into separate module

--- src/FullCardInfoUpdater.js
+++ src/FullCardInfoUpdater.js
@@ -1,4 +1,5 @@
 import Page from "./pages/page";
+import fixCardNameFontSize from "./utils/fixCardNameFontSize";
 
 export default class FullCardInfoUpdater extends Page {
   run() {
@@ -9,25 +10,7 @@ export default class FullCardInfoUpdater extends Page {
     }
 
     const fullCardInfoObserver = new MutationObserver(() => {
-      console.log('cardInfoContainer updated');
-      cardInfoContainer.classList.add('customElement');
-      const nameContainer = cardInfoContainer.querySelector('.name');
-
-      const nameClone = nameContainer.cloneNode(true);
-      nameClone.style.opacity = 0;
-      nameClone.style.whiteSpace = 'nowrap';
-      nameClone.style.width = 'fit-content';
-      nameClone.style.minWidth = '90%';
-      nameContainer.parentElement.append(nameClone);
-      let fontSize =
-        parseFloat(window.getComputedStyle(nameContainer).getPropertyValue('font-size'));
-      while ((nameContainer.clientWidth < nameClone.clientWidth || nameContainer.clientHeight > nameClone.clientHeight) && fontSize >= 1) {
-        fontSize -= 1;
-        nameClone.style.fontSize = fontSize + 'px';
-        nameContainer.style.fontSize = fontSize + 'px';
-      }
-      nameClone.remove();
-      console.log('finished');
+      fixCardNameFontSize(cardInfoContainer);
     });
 
     fullCardInfoObserver.observe(cardInfoContainer, { childList: true });
